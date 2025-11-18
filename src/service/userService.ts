0d9cecb3cf9cf85b306f81f17572ac5ad351763e
Mhangwani Ndima Karabo <203499810+Ndima-karabo17@users.noreply.github.com>
2025-11-19 01:53:04 +0200

    Update userService.ts
    I added a logout part

--- src/service/userService.ts
+++ src/service/userService.ts
@@ -1,16 +1,23 @@
 import {query} from '../config/database'
 import bcrypt from 'bcryptjs'
-import { Users } from '../types/users.types'
+import { user_role, Users } from '../types/users.types'
 
 export const findUserByEmail = async (email:string): Promise<Users|null> =>{
     const {rows}=  await query('SELECT * FROM Users WHERE email = $1', [email]);
     return rows[0] || null;
 }
+interface CreateUserInput {
+  name: string;
+  email: string;
+  phone_number: string;
+  role: user_role;
+  passsword_hash: string; 
+}
 
-export const createUsers = async (email:string, passsword_hash:string): Promise<Users> =>{
+export const createUsers = async ({name,email,phone_number,role, passsword_hash}:CreateUserInput): Promise<Users> =>{
 const salt = await bcrypt.genSalt(10);
 const passsword = await bcrypt.hash(passsword_hash, salt);
 
-const {rows} = await query('INSERT INTO (id, password) VALUES ($1, $2) RETURNING isd, password_hash', [email, passsword]);
-return rows[0];
+const {rows} = await query('INSERT INTO (name,email,phone_number,role, password) VALUES ($1, $2, $3,$4,$5, NOW(), NOW()) RETURNING name,email,phone_number,role, password', [name,email,phone_number,role, passsword]);
+return rows[0] as Users;
 }
