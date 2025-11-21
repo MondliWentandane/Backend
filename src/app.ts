c2d30f90593cceebeb58f9550ff2dca751591c19
Andile45 <manganyeandile@gmail.com>
2025-11-21 13:54:40 +0200

    I addded the Authetication routes

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,12 @@
 import express from "express"
 import testRoute from "./routes/testRoutes"
+import authRoutes from "./routes/authRoutes"
 
 const app = express();
 app.use(express.json());
+
+
+app.use("/api/auth",authRoutes)
 app.use("/api",testRoute);
 
 export default app;
