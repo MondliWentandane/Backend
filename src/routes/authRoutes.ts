710a38d249f33b36159330a5f6c19f79c5d9ac95
Andile45 <manganyeandile@gmail.com>
2025-11-26 11:46:11 +0200

    Update auth routes configuration

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,13 +1,14 @@
 import { Router } from "express";
-import { signUp,signIn,signInWithGoogle,forgottenPassword,resetPassword } from "../controllers/authController";
+import { signUp,signIn,signInWithGoogle,forgotPassword,resetPassword,refreshToken } from "../controllers/authController";
 
 const router = Router();
 
 router.post("/signup",signUp);
 router.post("/signin",signIn);
 router.post("/signin/google",signInWithGoogle);
-router.post("/forgot-password",forgottenPassword);
+router.post("/forgot-password",forgotPassword);
 router.post("/reset-password",resetPassword);
+router.post("/refresh-token",refreshToken);
 
 
 export default router;
